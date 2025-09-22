acece0fe0079411086252baa54ce5def6f4b9ee2
pedrovsfelix <seuemail@example.com>
2025-09-22 15:17:21 -0300

    :sparkles: feat: Conectando API ao MongoDB

---
+++ api/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const mongoURI = process.env.MONGODB_URI;
+
+if (!mongoURI) {
+  console.error('ERRO: A variável de ambiente MONGODB_URI não está definida.');
+  process.exit(1);
+}
+
+mongoose.connect(mongoURI)
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(port, () => {
+      console.log(`🚀 Server is running in http://localhost:${port}`)
+    });
+
+    console.log('Conectado com sucesso ao MongoDB Atlas!')
+  })
+  .catch(err => console.error('Erro ao conectar com o MongoDB Atlas:', err));
