b90b028c4842b2c8e4733c1f7db0e2d04b86f22d
pedrovsfelix <seuemail@example.com>
2025-09-23 08:39:02 -0300

    :sparkles: feat: Adicionando novas rotas

--- api/src/router.ts
+++ api/src/router.ts
@@ -5,6 +5,7 @@ import multer from 'multer';
 
 import { listCategories } from "./app/useCases/categories/listCategory.js";
 import { createCategories } from "./app/useCases/categories/createCategory.js";
+import { deleteCategory } from './app/useCases/categories/deleteCategory.js';
 import { listProducts } from "./app/useCases/products/listProducts.js";
 import { createProduct } from './app/useCases/products/createProduct.js';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory.js';
@@ -12,6 +13,7 @@ import { listOrders } from './app/useCases/orders/listOrders.js';
 import { createOrder } from './app/useCases/orders/createOrder.js';
 import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus.js';
 import { cancelOrder } from './app/useCases/orders/cancelOrder.js';
+import { changeProduct } from './app/useCases/products/changeProduct.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -34,11 +36,17 @@ router.get('/categories', listCategories)
 // Create category
 router.post('/categories', createCategories)
 
+// Delete category
+router.delete('/categories/:categoryId', deleteCategory)
+
 // List products
 router.get('/products', listProducts)
 
 // Create product
 router.post('/products', upload.single('image') ,createProduct)
+
+// Change product
+router.patch('/products/:productId', upload.single('image'), changeProduct)
 
 // Get product by category
 router.get('/categories/:categoryId/products', listProductsByCategory)
