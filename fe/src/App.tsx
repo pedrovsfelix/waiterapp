a2322ec2fc17b5f70bd1445ecf6712a2d454f584
pedrovsfelix <pedrovitorsf@gmail.com>
2025-09-23 22:55:08 -0300

    :sparkles: feat: Adicionando os pedidos na tela inicial

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./ui/components/Header";
 import Navbar from "./ui/components/Navbar";
+import Orders from "./ui/components/Orders";
 
 export default function App() {
   return (
-    <div className="flex">
+    <div className="flex h-dvh bg-white-light">
       <Navbar />
-      <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col ml-9 mt-10 mr-20 w-full gap-12">
         <Header />
+        <Orders />
       </div>
     </div>
   );
