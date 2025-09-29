bfb7729bc1a06b75ad51103adddf0b489b54b6fe
pedrovsfelix <seuemail@example.com>
2025-09-29 17:16:08 -0300

    :sparkles: feat: Integrando o board a API

--- fe/src/ui/components/OrdersBoard.tsx
+++ fe/src/ui/components/OrdersBoard.tsx
@@ -1,25 +1,59 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
+
 import type { Order } from "../../types/Order";
 import OrderModal from "./OrderModal";
+import { api } from "../../app/utils/api";
 
 interface OrdersBoardProps {
   icon: string;
   title: string;
   orders: Order[];
+  onCancelOrder(orderId: string): void;
+  onChangeOrderStatus(orderId: string, stauts: Order['status']): void;
 }
 
-export default function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
+export default function OrdersBoard({ icon, title, orders, onCancelOrder, onChangeOrderStatus }: OrdersBoardProps) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [ selectedOrder, setSelectedOrder ] = useState<null | Order>(null);
+  const [ isLoading, setIsloading ] = useState(false);
 
   function handleOpenModal(order: Order) {
     setIsModalVisible(true);
     setSelectedOrder(order);
   }
 
-    function handleCloseModal() {
+  function handleCloseModal() {
     setIsModalVisible(false);
     setSelectedOrder(null);
+  }
+
+  async function handleChangeOrderStatus() {
+    setIsloading(true);
+
+    const status = selectedOrder?.status === 'WAITING' ? 'IN_PRODUCTION' : 'DONE'
+
+    await api.patch(`/orders/${selectedOrder?._id}`, { status });
+
+    toast.success(`O pedido da mesa ${selectedOrder?.table} teve o status alterado!`);
+
+    onChangeOrderStatus(selectedOrder!._id, status);
+    setIsloading(false);
+    setIsModalVisible(false);
+
+  }
+
+  async function handleCancelOrder() {
+    setIsloading(true);
+
+    await api.delete(`/orders/${selectedOrder?._id}`);
+
+    toast.success(`O pedido da mesa ${selectedOrder?.table} foi cancelado!`);
+
+    onCancelOrder(selectedOrder!._id);
+    setIsloading(false);
+    setIsModalVisible(false);
+
   }
 
   return (
@@ -29,6 +63,9 @@ export default function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
         visible={isModalVisible}
         order={selectedOrder}
         onClose={handleCloseModal}
+        onCancelOrder={handleCancelOrder}
+        onChangeOrderStatus={handleChangeOrderStatus}
+        isLoading={isLoading}
       />
       <header className="flex items-center justify-center gap-2">
         <span>
