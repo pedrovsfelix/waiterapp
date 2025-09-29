d922082443705c8d8d1e543041d7ad451bd70b47
pedrovsfelix <seuemail@example.com>
2025-09-29 17:13:09 -0300

    :sparkles: feat: Adicionando Websockets

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
+import http from'node:http';
+import { Server } from 'socket.io';
+
 import { router } from './router.js';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -16,16 +19,28 @@ if (!mongoURI) {
   process.exit(1);
 }
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose.connect(mongoURI)
   .then(() => {
-    const app = express();
     const port = 3001;
 
+    io.emit('orders@new')
+
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+    })
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`🚀 Server is running in http://localhost:${port}`)
     });
 
