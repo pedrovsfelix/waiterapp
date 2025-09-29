f408e799552246d2e219ba8e1d609769d998947a
pedrovsfelix <seuemail@example.com>
2025-09-29 17:14:56 -0300

    :sparkles: feat: Listando as ordens de pedidos e adicionando websocket

--- fe/src/ui/components/Orders.tsx
+++ fe/src/ui/components/Orders.tsx
@@ -1,52 +1,69 @@
+import { useEffect, useState } from 'react';
+import socketIo from 'socket.io-client';
 import type { Order } from '../../types/Order';
 import OrdersBoard from './OrdersBoard';
-
-const orders: Order[] = [
-	{
-		'_id': '68d3369dd798c9590c55bbe2',
-		'table': '1',
-		'status': 'WAITING',
-		'products': [
-			{
-				'product': {
-					'name': 'Pizza 4 queijos',
-					'imagePath': '1758676796291-MHEB7V2WRFDVHBXPFP6RFYZ66U.png',
-					'price': 40,
-				},
-				'quantity': 2,
-				'_id': '68d3369dd798c9590c55bbe3'
-			},
-			{
-				'product': {
-					'name': 'Coca cola',
-					'imagePath': '1758588251107-210692-800-auto.png',
-					'price': 7,
-				},
-				'quantity': 2,
-				'_id': '68d3369dd798c9590c55bbe4'
-			}
-		],
-	}
-]
+import { api } from '../../app/utils/api';
 
 export default function Orders() {
+    const[ orders, setOrders ] = useState<Order[]>([]);
+
+    useEffect(() => {
+      const io = socketIo('http://localhost:3001', {
+        transports: ['websocket'],
+      });
+
+      io.on('orders@new', (order) => {
+        setOrders(prevState => prevState.concat(order));
+      });
+
+    }, []);
+
+    useEffect(() => {
+      api.get('/orders')
+        .then(({data}) => {
+          setOrders(data);
+        });
+    }, []);
+
+    const waiting = orders.filter((order) => order.status === 'WAITING');
+    const inProduction = orders.filter((order) => order.status === 'IN_PRODUCTION');
+    const done = orders.filter((order) => order.status === 'DONE');
+
+    function handleCancelOrder(orderId: string) {
+      setOrders((prevState) => prevState.filter(order => order._id !== orderId))
+    }
+
+    function handleOrderStatusChange(orderId: string, status: Order['status']) {
+      setOrders((prevState) => prevState.map((order) => (
+        order._id === orderId
+          ? { ...order, status }
+          : order
+      )));
+    }
+
     return (
       <div className='flex gap-8'>
 
         <OrdersBoard
           icon='🕛'
           title='Fila de espera'
-          orders={orders}
+          orders={waiting}
+          onCancelOrder={handleCancelOrder}
+          onChangeOrderStatus={handleOrderStatusChange}
         />
         <OrdersBoard
           icon='🧑🏼‍🍳'
           title='Em preparação'
-          orders={[]}
+          orders={inProduction}
+          onCancelOrder={handleCancelOrder}
+          onChangeOrderStatus={handleOrderStatusChange}
         />
         <OrdersBoard
           icon='✅'
           title='Pronto'
-          orders={[]}
+          orders={done}
+          onCancelOrder={handleCancelOrder}
+          onChangeOrderStatus={handleOrderStatusChange}
         />
 
       </div>
