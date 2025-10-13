cf51d02e273a85a2cf4f1cc4d6747db75ce18f58
pedrovsfelix <seuemail@example.com>
2025-10-13 17:18:23 -0300

    :sparkles: feat: Integrando backend da aplicação.

---
+++ fe/src/app/services/httpClient.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { localStorageKeys } from "../config/localStorageKeys";
+
+export const httpClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+})
+
+httpClient.interceptors.request.use(async config => {
+  const accessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+
+  if(accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config
+})
+
+httpClient.interceptors.response.use(async data => {
+  return data;
+})
