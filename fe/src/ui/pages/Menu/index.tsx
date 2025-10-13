fb5ba8a7f5e7701fcf2486b56cc31f93adc4cc1e
pedrovsfelix <seuemail@example.com>
2025-10-13 17:31:17 -0300

    :recycle: refactor: Listando rota dos produtos.

--- fe/src/ui/pages/Menu/index.tsx
+++ fe/src/ui/pages/Menu/index.tsx
@@ -1,7 +1,11 @@
+import Header from "../../components/Header";
+import Products from "../../components/Products";
+
 export default function Menu() {
   return (
     <>
-      <h1>Menu</h1>
+      <Header />
+      <Products />
     </>
   )
 }
