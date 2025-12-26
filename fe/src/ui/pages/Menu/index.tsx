aff4071603569b1042253bb44085a579707479e1
pedrovsfelix <pedrovitorsf@gmail.com>
2025-12-26 16:48:43 -0300

    :recycle: refactor: Refatorando header da página de Cardápio.

--- fe/src/ui/pages/Menu/index.tsx
+++ fe/src/ui/pages/Menu/index.tsx
@@ -1,10 +1,15 @@
 import Header from "../../components/Header";
 import Products from "../../components/Products";
+import SvgMenu from "../../icons/Menu";
 
 export default function Menu() {
   return (
     <>
-      <Header />
+      <Header
+        icon={<SvgMenu className="w-5 h-5" />}
+        title="Cardápio"
+        description="Gerencie os produtos do seu estabelecimento"
+      />
       <Products />
     </>
   )
