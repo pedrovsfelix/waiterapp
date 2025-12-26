b40862abae16181002bdd8a0896b3586dfee510b
pedrovsfelix <pedrovitorsf@gmail.com>
2025-12-26 16:36:48 -0300

    :recycle: refactor: Listando todas as rotas de API no frontend.

--- fe/src/app/services/users/index.ts
+++ fe/src/app/services/users/index.ts
@@ -1,5 +1,11 @@
 import { me } from "./me";
+import { meProfile } from "./meProfile";
+import { remove } from "./remove";
+import { update } from "./update";
 
 export const usersServices = {
   me,
+  meProfile,
+  remove,
+  update
 }
