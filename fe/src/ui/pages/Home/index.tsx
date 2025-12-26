99e598e593a08aa3bcb3cadb7f5d0ead167a8f94
pedrovsfelix <pedrovitorsf@gmail.com>
2025-12-26 16:48:08 -0300

    :recycle: refactor: Refatorando header da página Home.

--- fe/src/ui/pages/Home/index.tsx
+++ fe/src/ui/pages/Home/index.tsx
@@ -1,10 +1,27 @@
+import Button from "../../components/Button";
 import Header from "../../components/Header";
 import Orders from "../../components/Orders";
+import { Refresh } from "../../icons";
+import SvgHome from "../../icons/Home";
 
 export default function Home() {
   return (
     <>
-        <Header />
+        <Header
+          icon={<SvgHome className="w-5 h-5 text-black"/>}
+          title="Home"
+          description="Acompanhe os pedidos dos clientes"
+          rightAction={
+            <div>
+              <Button
+                secondary
+                icon={<Refresh className="w-5 h-5"/>}
+              >
+                Reiniciar o dia
+              </Button>
+            </div>
+          }
+        />
         <Orders />
     </>
   );
