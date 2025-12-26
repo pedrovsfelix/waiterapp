fa853b280c18254ccc3421bc5aa85a6a8a81d0a7
pedrovsfelix <pedrovitorsf@gmail.com>
2025-12-26 16:42:18 -0300

    :recycle: refactor: Refatorando propriedade do icone de Atualizar.

--- fe/src/ui/icons/Refresh.tsx
+++ fe/src/ui/icons/Refresh.tsx
@@ -8,23 +8,24 @@ const SvgRefresh = (props: SVGProps<SVGSVGElement>) => (
     fill="none"
     viewBox="0 0 24 24"
     {...props}
+    stroke="currentColor"
   >
     <path
-      stroke="#666"
+
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={1.5}
       d="M12 20.003a8 8 0 0 0 7.03-4.177M20.003 12A8.003 8.003 0 0 0 12 3.997M12 3.997a8 8 0 0 0-7.03 4.176M3.997 12A8.003 8.003 0 0 0 12 20.003"
     />
     <path
-      stroke="#666"
+
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={1.5}
       d="M4.97 8.173A8.003 8.003 0 0 1 20.004 12M3.997 12a8.003 8.003 0 0 0 15.033 3.827"
     />
     <path
-      stroke="#666"
+
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={1.5}
